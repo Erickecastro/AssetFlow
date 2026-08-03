984308b960b3eec971cf9c92ed68355d828a58dd
Ericke <eerickevh@gmail.com>
2026-08-03 13:28:11 -0400

    feat: add asset condition enum

---
+++ src/AssetFlow.Domain/Enums/AssetCondition.cs
@@ -0,0 +1,12 @@
+namespace AssetFlow.Domain.Enums;
+
+public enum AssetCondition
+{
+    New = 1,
+    Excellent = 2,
+    Good = 3,
+    Fair = 4,
+    Poor = 5,
+    Damaged = 6,
+    Unserviceable = 7
+}
