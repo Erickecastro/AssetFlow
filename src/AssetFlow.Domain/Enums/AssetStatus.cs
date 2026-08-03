efe5faec1662868dd8b7131ec8ffe769bd1c7d02
Ericke <eerickevh@gmail.com>
2026-08-03 13:22:20 -0400

    feat: add asset status enum

---
+++ src/AssetFlow.Domain/Enums/AssetStatus.cs
@@ -0,0 +1,12 @@
+namespace AssetFlow.Domain.Enums;
+
+public enum AssetStatus
+{
+    Available = 1,
+    Assigned = 2,
+    Reserved = 3,
+    UnderMaintenance = 4,
+    Retired = 5,
+    Lost = 6,
+    Disposed = 7
+}
